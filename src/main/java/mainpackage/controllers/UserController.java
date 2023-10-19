98d069d7096ca0c14ff1d213c8047534f6b61d2e
kvint <kvint1985@gmail.com>
2023-10-20 02:43:21 +0300

    add user's logic

--- src/main/java/mainpackage/controllers/UserController.java
+++ src/main/java/mainpackage/controllers/UserController.java
@@ -6,6 +6,7 @@ import mainpackage.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -38,5 +39,12 @@ public class UserController {
     @GetMapping("/hello")
     public String securityUrl(){
         return "hello";
+    }
+
+    @GetMapping("/user/{user}")
+    public String userInfo(@PathVariable("user") User user, Model model){
+        model.addAttribute("user", user);
+        model.addAttribute("products", user.getProducts());
+        return "user-info";
     }
 }
